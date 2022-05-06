54917f4a9db89c8b3e230f8455d2f25b55232620
Robby Pambudi <robby.pambudi10@gmail.com>
2022-05-07 01:21:38 +0700

    authorization implemented

--- src/services/postgres/NotesService.js
+++ src/services/postgres/NotesService.js
@@ -1,7 +1,10 @@
+/* eslint no-underscore-dangle: 0 */
+
 const { nanoid } = require('nanoid');
 const { Pool } = require('pg');
 const InvariantError = require('../../exceptions/InvariantError');
 const NotFoundError = require('../../exceptions/NotFoundError');
+const AuthorizationError = require('../../exceptions/AuthorizationError');
 const { mapDBToModel } = require('../../utils');
 
 class NotesService {
@@ -9,15 +12,31 @@ class NotesService {
         this._pool = new Pool();
     }
 
-    async addNotes({ title, body, tags }) {
+    async verifyNoteOwner(id, owner) {
+        const query = {
+            text: 'SELECT * FROM notes WHERE id = $1',
+            values: [id],
+        };
+        const result = await this._pool.query(query);
+        if (!result.rows.length) {
+            throw new NotFoundError('Catatan tidak ditemukan');
+        }
+        const note = result.rows[0];
+        if (note.owner !== owner) {
+            throw new AuthorizationError('Anda tidak berhak mengakses resource ini');
+        }
+    }
+
+    // eslint-disable-next-line object-curly-newline
+    async addNote({ title, body, tags, owner }) {
         const id = nanoid(16);
         const createdAt = new Date().toISOString();
         const updatedAt = createdAt;
 
         // Query
         const query = {
-            text: `INSERT INTO notes VALUES ($1, $2, $3, $4, $5, $6) RETURNING id`,
-            values: [id, title, body, tags, createdAt, updatedAt],
+            text: 'INSERT INTO notes VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id',
+            values: [id, title, body, tags, createdAt, updatedAt, owner],
         };
 
         const result = await this._pool.query(query);
@@ -29,14 +48,18 @@ class NotesService {
         return result.rows[0].id;
     }
 
-    async getNotes() {
-        const result = await this._pool.query('SELECT * FROM notes');
+    async getNotes(owner) {
+        const query = {
+            text: 'SELECT * FROM notes WHERE owner = $1',
+            values: [owner],
+        };
+        const result = await this._pool.query(query);
         return result.rows.map(mapDBToModel);
     }
 
     async getNoteById(id) {
         const query = {
-            text: `SELECT * FROM notes WHERE id = $1`,
+            text: 'SELECT * FROM notes WHERE id = $1',
             values: [id],
         };
         const result = await this._pool.query(query);
@@ -50,7 +73,7 @@ class NotesService {
     async editNoteById(id, { title, body, tags }) {
         const updatedAt = new Date().toISOString();
         const query = {
-            text: `UPDATE notes SET title = $1, body = $2, tags = $3, updated_at = $4 WHERE id = $5 RETURNING id`,
+            text: 'UPDATE notes SET title = $1, body = $2, tags = $3, updated_at = $4 WHERE id = $5 RETURNING id',
             values: [title, body, tags, updatedAt, id],
         };
         const result = await this._pool.query(query);
@@ -60,9 +83,10 @@ class NotesService {
         }
         // return result.rows[0].id;
     }
+
     async deleteNoteById(id) {
         const query = {
-            text: `DELETE FROM notes WHERE id = $1 RETURNING id`,
+            text: 'DELETE FROM notes WHERE id = $1 RETURNING id',
             values: [id],
         };
         const result = await this._pool.query(query);
